c0b1a98ebf3fd300630359669ac64edd585ec185
Kelvin Moi <kelvin.mai002@gmail.com>
2017-11-24 10:30:01 -0600

    Completed modal functionality

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,4 +1,4 @@
-import $ from 'jquery'
+import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 import smoothScroll from 'jquery-smooth-scroll';
 
